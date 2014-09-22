12a0737098b050183d4c8489a8463c58bbbac0a6
jack.lin <jack20039@gmail.com>
2014-09-22 14:58:46 +0800

    Update get_book_from_kanunu.php

--- get_book_from_kanunu.php
+++ get_book_from_kanunu.php
@@ -28,7 +28,7 @@ for($i=$start;$i<=$end;$i++){
 //Using sublime to edit source html code in this page, deleting images and tags and js you don't need for reading.
 //then change <meta charset='utf-8'/>; 
 //use wkhtmltopdf to generate the book : wkhtmltopdf --no-images --no-background -n http:localhost/xxx  book_name.pdf
-//find the book_name.pdf in my document dir.
+//find the book_name.pdf in C:\Users\jack.lin dir.
 
 //2014/6/14 Q: font size in the pdf is too small to read
 /**
