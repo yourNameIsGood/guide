0f84d42044e5072e82dd501f12fd793007be6705
jack.lin <jack20039@gmail.com>
2014-10-14 16:45:22 +0800

    删除七牛bucket中的文件

--- qiniu_api_get_filelist_in_bucket.php
+++ qiniu_api_get_filelist_in_bucket.php
@@ -1,13 +1,15 @@
 <?php
-//2014-08-17  获取文件bucket下 文件list
-
+//2014-08-17  获取文件bucket下 文件list  
+//2014-10-14  按照list清空bucket内的文件
 require_once("rs.php");
+require_once("io.php");
+require_once("fop.php"); // file operation
 
 class qiniu{
-    private $bucket = 'xxxxxx';
+    private $bucket = 'crmwaijiao';
     private $domain = '';
-    private $secretKey = 'xxxxxx';
-    private $accessKey = 'xxxxxx';
+    private $secretKey = '9p22wIJcEPg3vXf2LmvOfAG8sEXoE0GbsI3Hzlq2';
+    private $accessKey = '9bCt2CbRL9H0CZ8S4NGzafglPibha9pT-0wdHgLB';
 
     function __construct(){
         $this->domain = "{$this->bucket}.qiniudn.com";
@@ -32,6 +34,18 @@ class qiniu{
         return $data;
     }
 
+    function qiniu_del($key){
+        $bucket = $this->bucket;
+        $domain = $this->domain;
+        $client = new Qiniu_MacHttpClient(null);
+        $getPolicy = new Qiniu_RS_GetPolicy();
+        $baseUrl = Qiniu_RS_MakeBaseUrl($domain, $key);
+        $privateUrl = $getPolicy->MakeRequest($baseUrl, null);
+        $client = new Qiniu_MacHttpClient(null);
+        $err = Qiniu_RS_Delete($client, $bucket, $key);
+        var_dump($err);
+    }
+
     public function listfiles($marker=null,$limit=1000){
         $client = new Qiniu_MacHttpClient(null);
         $sign = "/list?bucket={$this->bucket}&marker={$marker}&limit={$limit}\n";//不能少了backslash + n 否则签名不正确
@@ -42,17 +56,26 @@ class qiniu{
         $arr = json_decode($return,true);
         $arr['count_items'] = count($arr['items']);
         $arr['marker'] = isset($arr['marker'])?$arr['marker']:'';
-        file_put_contents('list_log.txt',date('H:i:s',time())." marker: {$arr['marker']} || count={$arr['count_items']}".PHP_EOL, FILE_APPEND|LOCK_EX);
+        file_put_contents('production-list_log.txt',date('H:i:s',time())." marker: {$arr['marker']} || count={$arr['count_items']}".PHP_EOL, FILE_APPEND|LOCK_EX);
         foreach($arr['items'] as $val){
-            file_put_contents('files.txt',"{$val['key']}".PHP_EOL, FILE_APPEND|LOCK_EX);
+            file_put_contents('production-files.txt',"{$val['key']}".PHP_EOL, FILE_APPEND|LOCK_EX);
         }
+        
+        //2014-10-14  删除bucket内的所有文件
+        $files = ($arr['items']);
+        if($files){
+            foreach($files as $val){
+                $key = $val['key'];
+                $this->qiniu_del($key);
+            }
+        }
+        //删除文件  over
+
         $return = json_encode($arr);
         echo ($return);die;
     }
-
- 
 }
 
 $qn = new qiniu();
-$m = "eyJjIjowLCJrIjoid2hlcmVfZG9feW91X3VzdWFsbHlfcGxheV9iZWlzaGlkYS5tcDMifQ==";
+$m = ""; //如果这里有值，下次获取list的时候就从这个标记开始
 $url = $qn->listfiles($m);
