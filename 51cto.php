9933855f14ddd72428023f4a795f1ea42c0d6623
jack.lin <jack20039@gmail.com>
2014-10-15 14:26:13 +0800

    change request interval  and  key point

--- 51cto.php
+++ 51cto.php
@@ -7,10 +7,12 @@
         curl_setopt($ch, CURLOPT_URL,$url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36";
-        curl_setopt($ch, CURLOPT_USERAGENT, $agent); // prevent know as a robot
-        $ref = "http://linkindick.blog.51cto.com/2345165/1319045"; // the actual article addr
-        curl_setopt($ch, CURLOPT_AUTOREFERER, $ref); //
+        
+        //useless
+        //$agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36";
+        //curl_setopt($ch, CURLOPT_USERAGENT, $agent); // prevent know as a robot
+        //$ref = "http://linkindick.blog.51cto.com/2345165/1319045"; // the actual article addr
+        //curl_setopt($ch, CURLOPT_AUTOREFERER, $ref); //
 
 
         curl_setopt($ch, CURLOPT_POST, 1);//make it a post request
@@ -21,10 +23,10 @@
     }
 
     $url = "http://linkindick.blog.51cto.com/js/header.php";
-    $param = array('uid'=>2345165,'tid'=>1319045);
+    $param = array('uid'=>2345165,'tid'=>1319045); //key
     for($a=0;$a<3000;$a++){
       echo $a+1;
       $res = request_post($url, $param);
       var_dump($res);
-      usleep(25000); // 0.25s per request
+      usleep(22000); // 0.22s per request.
     }
