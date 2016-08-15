0494e34aec6e3e39765caa104719e07e019542f3
jack20039 <jack20039@gmail.com>
2016-08-15 10:03:48 +0800

    backup bashrc

--- myShell/codeBackup.sh
+++ myShell/codeBackup.sh
@@ -1,3 +1,6 @@
 # update  CrontabAction.class.php
 cd ~/work/code/soc_portal/soc_portal/trunk/App/Api/Action 
 cp -r CrontabAction.class.php ~/work/diary/backup
+
+cd ~/work/code/soc_portal/soc_portal/trunk/App/Api/Action/
+cp -rf Dataproxy ~/work/diary/backup/
