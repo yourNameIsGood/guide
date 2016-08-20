15c8707acedc961fb29dc98663d95d1846d7226c
yourNameIsGood <jack20039@gmail.com>
2016-08-20 11:54:03 +0800

    optimize lantern shell

--- myShell/lantern_proxy.sh
+++ myShell/lantern_proxy.sh
@@ -1,6 +1,10 @@
 
 cd /c/windows/System32/ && echo inetcpl.cpl | cmd
 
+cd /c/Users/hp/Desktop/ && echo Lantern.lnk | cmd
 
 t=$(date +%s)
+echo " "
+echo " "
+#echo "\r"
 echo "http://127.0.0.1:16823/proxy_on.pac?"$t"123456789"
