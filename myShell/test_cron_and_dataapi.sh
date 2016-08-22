295d245f90cf405a646f55fe9b9eeb7b21bbd5ed
jack20039 <jack20039@gmail.com>
2016-08-22 14:57:31 +0800

    optimize test shell

--- myShell/test_cron_and_dataapi.sh
+++ myShell/test_cron_and_dataapi.sh
@@ -1,9 +1,13 @@
 st=1470882396
 et=1470885996
+fpath=/tmp/
+
 cd ~/work/code/soc_portal/soc_portal/trunk/www
-php crontab.php backup_dataapi/starttime/${st}/endtime/${et}/interval/1min
-sleep 3
-php crontab.php test_cache_backup/data_origin/cloud/spe_gid/5/starttime/${st}/endtime/${et}/interval/1min  > /tmp/cache.json && python -m json.tool /tmp/cache.json > /tmp/2 
-curl http://logapi.intra.nexqloud.net:8002/webapi/ -d '''{ "jsonrpc": "2.0", "method": "spe_mitigation_traffic", "params": ["5","", "cloud", 1470882396, 1470885996, "1min"], "id": "aSUjSDRsmws6Fju1"}''' > /tmp/api.json && sleep 1 && python -m json.tool /tmp/api.json > /tmp/1
-sed -i 's/\.0//' /tmp/1
-vimdiff /tmp/1 /tmp/2 
+php crontab.php backup_dataapi/starttime/${st}/endtime/${et}/interval/1min && sleep 3  # get all the data from data-api and write it into mongodb
+# query one data from mongodb
+php crontab.php test_cache_backup/data_origin/cloud/spe_gid/5/starttime/${st}/endtime/${et}/interval/1min  > ${fpath}cache.json && python -m json.tool ${fpath}cache.json > ${fpath}2 
+# query one data from data-api
+curl http://logapi.intra.nexqloud.net:8002/webapi/ -d '''{ "jsonrpc": "2.0", "method": "spe_mitigation_traffic", "params": ["5","", "cloud", '''${st}''','''${et}''', "1min"], "id": "aSUjSDRsmws6Fju1"}''' > ${fpath}api.json && sleep 1 && python -m json.tool ${fpath}api.json > ${fpath}1
+# get rid of .0 in return of data-api
+sed -i 's/\.0//' ${fpath}1
+vimdiff ${fpath}1 ${fpath}2 
