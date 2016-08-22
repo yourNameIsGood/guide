29c07e27a3206e4686fef63eef5bffa96b7d3901
jack20039 <jack20039@gmail.com>
2016-08-22 09:58:47 +0800

    code backup edit

--- myShell/codeBackup.sh
+++ myShell/codeBackup.sh
@@ -1,10 +1,13 @@
 # backup soc_portal crontab code
 cd ~/work/code/soc_portal/soc_portal/trunk/App/Api/Action 
-cp -r CrontabAction.class.php ~/work/diary/backup
+cp -f CrontabAction.class.php ~/work/diary/backup/ng/
+#cd ~/work/code/soc_portal/soc_portal/trunk/App/Api/Action/
+cp -rf Dataproxy ~/work/diary/backup/ng/
 
-cd ~/work/code/soc_portal/soc_portal/trunk/App/Api/Action/
-cp -rf Dataproxy ~/work/diary/backup/
+cd ~/work/code/soc_portal/soc_portal/trunk/App/Api/
+cp -rf Model ~/work/diary/backup/ng/
 
 # backup ~/.bashrc in Nexusguard
-d=$(date +%F)
-cp -f ~/.bashrc ~/work/diary/backup/.bashrc.from.NexusGuard.$d
+#d=$(date +%F)
+cp -f ~/.bashrc ~/work/diary/backup/ng/.bashrc.from.NexusGuard
+
