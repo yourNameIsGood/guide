17169bb94e46d7bbb82d1090376b54d9a55908d4
jack20039 <jack20039@gmail.com>
2016-08-25 17:23:45 +0800

    2016年 08月 25日 星期四 17:23:45 CST

--- myShell/codeBackup.sh
+++ myShell/codeBackup.sh
@@ -1,18 +1,27 @@
-# backup soc_portal crontab code
+msg="$1"
+if [ $# -eq 0 ]
+    then
+        msg=$(date)
+fi
+
+################################
+# backup soc_portal crontab code  
+################################
 cp -f ~/work/code/soc_portal/soc_portal/trunk/App/Api/Action/CrontabAction.class.php ~/work/diary/backup/ng/
 cp -rf ~/work/code/soc_portal/soc_portal/trunk/App/Api/Action/Dataproxy ~/work/diary/backup/ng/
 
 cd ~/work/code/soc_portal/soc_portal/trunk/App/Api/
 cp -rf Model ~/work/diary/backup/ng/
 
-cd ~/work/diary
-sh /home/randylin/work/code/guide/myShell/commit_and_push.sh "$1"
+cd ~/work/diary/
+sh /home/randylin/work/code/guide/myShell/commit_and_push.sh "${msg}"
 
-##############################################
-# backup some config in Nexusguard
-#cp -f ~/.bashrc ~/work/code/guide/myShell/nxgbak/.bashrc
-#cp -f ~/.vimrc ~/work/code/guide/myShell/nxgbak/.vimrc
-#cp -f ~/.tmux.conf ~/work/code/guide/myShell/nxgbak/.tmux.conf
-#
-#cd ~/work/code/guide/
-#sh /home/randylin/work/code/guide/myShell/commit_and_push.sh "$1"
+#####################################
+#backup some config in Nexusguard
+#####################################
+cp -f ~/.bashrc ~/work/code/guide/myShell/nxgbak/.bashrc
+cp -f ~/.vimrc ~/work/code/guide/myShell/nxgbak/.vimrc
+cp -f ~/.tmux.conf ~/work/code/guide/myShell/nxgbak/.tmux.conf
+
+cd ~/work/code/guide/
+sh /home/randylin/work/code/guide/myShell/commit_and_push.sh "${msg}"
