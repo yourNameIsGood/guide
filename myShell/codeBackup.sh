a6ec974a185bdf1bd5e6d82a294e6461742258f5
jack20039 <jack20039@gmail.com>
2016-09-19 10:43:19 +0800

    2016年 09月 19日 星期一 10:39:22 CST

--- myShell/codeBackup.sh
+++ myShell/codeBackup.sh
@@ -10,8 +10,13 @@ fi
 cp -f ~/work/code/soc_portal/soc_portal/trunk/App/Api/Action/CrontabAction.class.php ~/work/diary/backup/ng/
 cp -rf ~/work/code/soc_portal/soc_portal/trunk/App/Api/Action/Dataproxy ~/work/diary/backup/ng/
 
+# back up soc project
 cd ~/work/code/soc_portal/soc_portal/trunk/App/Api/
 cp -rf Model ~/work/diary/backup/ng/
+
+# back up Project Data api
+cd ~/work/code/data_api/trunk/
+cp -rf l7datawebapi ~/work/diary/backup/ng/
 
 cd ~/work/diary/
 sh /home/randylin/work/code/guide/myShell/commit_and_push.sh "${msg}"
