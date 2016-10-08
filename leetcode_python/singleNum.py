62878de564c63aec4c2a5c6f2ca04a239c4a765d
jack20039 <jack20039@gmail.com>
2016-10-08 16:30:27 +0800

    2016年 10月 08日 星期六 16:30:01 CST

--- leetcode_python/singleNum.py
+++ leetcode_python/singleNum.py
@@ -1,9 +1,19 @@
 def single(nums):
     for val in enumerate(nums):
         x = nums[0]
+        print "nums"
+        print nums
         del(nums[0])
         if x not in nums:
+            print str(x) + ' not in !!'
             return x
+        else:
+            for i in range(0,len(nums)):
+                print 'for loop ' + str(i)
+                print nums
+                print nums[i],x
+                if nums[i]==x:
+                    del(nums[i])
 
 n = [1,0,1]
 print single(n)
