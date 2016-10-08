9030d9e89a33dc4d2bfcef6b4358bd7fd5afde76
jack20039 <jack20039@gmail.com>
2016-10-08 15:30:27 +0800

    2016年 10月 08日 星期六 15:30:01 CST

--- leetcode_python/addTwoNumbers.py
+++ leetcode_python/addTwoNumbers.py
@@ -1,15 +1,51 @@
-def addTwoNumbers(l1, l2):
-    n1 = map(str,l1)
-    n = ''.join(n1)
-    n1 = int(n)
-    n2 = map(str,l2)
-    n = ''.join(n2)
-    n2 = int(n)
-    total = n1 + n2
-    l = map(int, str(total))
-    l = l[::-1]
-    print l
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-l1 = [2,4,3]
-l2 = [5,6,4]
-addTwoNumbers(l1,l2)
+    # Define this to check if it works well
+    def myPrint(self):
+        if self.val != None:
+            print(self.val)
+        if self.next:
+            self.next.myPrint()
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        result = ListNode(0);
+        cur = result;
+        while l1 or l2:
+           cur.val += self.addTwoNodes(l1, l2)
+           if cur.val >= 10:
+               cur.val -= 10
+               cur.next = ListNode(1)
+           else:
+               # Check if there is need to make the next node
+               if l1 and l1.next or l2 and l2.next:
+                   cur.next = ListNode(0)
+           cur = cur.next
+           if l1:
+               l1 = l1.next
+           if l2:
+               l2 = l2.next
+           return result
+
+    def addTwoNodes(self, n1, n2):
+        if not n1 and not n2:
+            # This cannot happen, ignore it
+            None
+        if not n1:
+            return n2.val
+        if not n2:
+            return n1.val
+        return n1.val + n2.val
+
+
+if __name__ == "__main__":
+    list1 = ListNode(5)
+    list1.next = ListNode(8)
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(3)
+    print(l2.myPrint())
+    #print(Solution().addTwoNumbers(list1, l2).myPrint())
