312c0343c81641697de3dbe6d5e9a8e18d13e7a0
jack20039 <jack20039@gmail.com>
2016-10-08 13:00:29 +0800

    2016年 10月 08日 星期六 13:00:01 CST

--- leetcode_python/addTwoNumbers.py
+++ leetcode_python/addTwoNumbers.py
@@ -1,7 +1,14 @@
 def addTwoNumbers(l1, l2):
-    n1 = l1[::-1]
-    int1 = ''.join(n1)
-    print int1
+    n1 = map(str,l1)
+    n = ''.join(n1)
+    n1 = int(n)
+    n2 = map(str,l2)
+    n = ''.join(n2)
+    n2 = int(n)
+    total = n1 + n2
+    l = map(int, str(total))
+    l = l[::-1]
+    print l
 
 l1 = [2,4,3]
 l2 = [5,6,4]
