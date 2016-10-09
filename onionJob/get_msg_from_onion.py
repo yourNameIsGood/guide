c610006313944415bf6b8e7b5ab155b2197a5c47
jack20039 <jack20039@gmail.com>
2016-10-09 14:50:28 +0800

    2016年 10月 09日 星期日 14:50:01 CST

--- onionJob/get_msg_from_onion.py
+++ onionJob/get_msg_from_onion.py
@@ -5,18 +5,28 @@ import pycurl
 import curl
 import json
 import os
+import sys
 
 if __name__ == "__main__":
     #for i in range(1,290):
-    for i in range(288,290):
+    for i in range(1,288):
         url = "https://coding.net/api/tweet_topic/"+str(i)
         res = curl.get(url)
         data = json.loads(res)
+        filename = "slogan"
         if data['code'] == 0 :
             user_list = data['data']['user_list']
-            filename = "slogan"
+            print type(user_list)
+            sys.exit(0)
             for i in user_list:
                 slogan = i['slogan']
                 if slogan:
                     slogan = slogan.encode('utf-8')
                     os.system(" echo \" " + str(slogan) + "\" >> " + filename)
+            if 'hot_tweet' in data:
+                user_list = data['data']['hot_tweet']['comment_list']
+                for i in user_list:
+                    slogan = i['owner']['slogan']
+                    if slogan:
+                        slogan = slogan.encode('utf-8')
+                        os.system(" echo \" " + str(slogan) + "\" >> " + filename) 
