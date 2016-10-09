a2b5e46f8d1bf31953ec7cf826b87b5c28f9e4f3
jack20039 <jack20039@gmail.com>
2016-10-09 16:40:32 +0800

    2016年 10月 09日 星期日 16:40:01 CST

--- onionJob/run.py
+++ onionJob/run.py
@@ -11,6 +11,7 @@ import job
 
 if __name__ == "__main__":
     f = open('today_done_account','r')
+    filename ='today_done_account'
     today_done_account = f.readlines()
     f.close()
     f = open("uniq_slogan",'r')
@@ -20,6 +21,7 @@ if __name__ == "__main__":
     for a in acc:
         uid = str(a)
         email = acc[a]
+        print uid, email
         if email in today_done_account:
             print str(email) + " is done already "
             continue
@@ -32,7 +34,9 @@ if __name__ == "__main__":
             topic = ("#"+topic+"#") if topic else topic
             con = slogan.pop()
             content = topic  + con
-            print "job doing " , email,content
+            print "\n job doing " , email,content
             time.sleep(10)
             mp = job.maopao(email, content) 
+            if mp:
+                os.system(" echo \" " + str(email) + "\" >> " + filename) 
             break
