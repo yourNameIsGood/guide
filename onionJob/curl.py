846b14152a20b46108fa12f7835c8264a2e2d8d4
jack20039 <jack20039@gmail.com>
2016-10-09 10:50:32 +0800

    2016年 10月 09日 星期日 10:50:01 CST

--- onionJob/curl.py
+++ onionJob/curl.py
@@ -1,6 +1,7 @@
 import pycurl
 import os.path
 import time
+import accounts 
 
 def post(url,data=None, cookie_path=None):
     c = pycurl.Curl()
@@ -18,3 +19,13 @@ def post(url,data=None, cookie_path=None):
     if(statusCode != 200):
         print 'statusCode' + str(statusCode)
     return True
+
+
+if __name__ == "__main__":
+    url = "https://coding.net/api/login"
+    login_data = "password=403ce753c041efda97535bdfbcf836ea7d20215d&remember_me=false&email="
+    print acc.test_account
+
+    for a in acc.test_account:
+        print 
+    #post(url)
