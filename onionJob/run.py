4e6e9a0bb2d0c513aee7e98b2877b47d60ecbe93
jack20039 <jack20039@gmail.com>
2016-10-09 16:10:27 +0800

    2016年 10月 09日 星期日 16:10:01 CST

--- onionJob/run.py
+++ onionJob/run.py
@@ -6,7 +6,21 @@ if __name__ == "__main__":
     today_done_account = f.readlines()
     f.close()
     acc = account.all_account
-    for i in acc:
-        if acc in today_done_account:
+    for a in acc:
+        uid = str(a)
+        email = acc[a]
+        if email in today_done_account:
+            print str(email) + " is done already "
             continue
+        else:
+            login_res = job.login(email)
+            if not login_res:
+                print "login ERROR !!!"
+                break;
+            topic = "#" + "" + "#"
+            con = slogan.pop()
+            content = topic  + con
+            print email,content
+            time.sleep(10)
+            mp = job.maopao(email, content) 
 
